4e7eb64c1e9ea10c5b38e81adcefc602f266b1dc
bigrpg <bigrpg@163.com>
2018-04-02 20:10:56 +0800

    init files

---
+++ UnityProject/Assets/Scenes/BasicAssetPipe.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+using UnityEngine.Experimental.Rendering;
+
+[ExecuteInEditMode]
+public class BasicAssetPipe : RenderPipelineAsset
+{
+    public Color clearColor = Color.green;
+
+#if UNITY_EDITOR
+    [UnityEditor.MenuItem("SRP-Demo/01 - Create Basic Asset Pipeline")]
+    static void CreateBasicAssetPipeline()
+    {
+        var instance = ScriptableObject.CreateInstance<BasicAssetPipe>();
+        UnityEditor.AssetDatabase.CreateAsset(instance, "Assets/Scenes/BasicAssetPipe.asset");
+    }
+#endif
+
+    protected override IRenderPipeline InternalCreatePipeline()
+    {
+        return new BasicAssetPipeInstance(clearColor);
+    }
+}
+
+public class BasicAssetPipeInstance : RenderPipeline
+{
+    private Color m_ClearColor = Color.black;
+
+    public BasicAssetPipeInstance(Color clearColor)
+    {
+        m_ClearColor = clearColor;
+    }
+
+    public override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        base.Render(context, cameras);
+
+        // clear buffers to the configured color
+        var cmd = new CommandBuffer();
+        cmd.ClearRenderTarget(true, true, m_ClearColor);
+        context.ExecuteCommandBuffer(cmd);
+        cmd.Release();
+        context.Submit();
+    }
+}
